f5f4f28e8921b7aa74a04ed1ee1de05fa073b185
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-02-26 15:15:42 +0100

    Adjusting the test element's initial position

--- classes.py
+++ classes.py
@@ -4,12 +4,12 @@ from pygame.math import Vector2
 class ELEMENT:
     """ Will contain element shapes and colors and functions for drawing the elements"""
     def __init__(self) -> None:
-        self.position = (13,4)
+        self.position = (4,2)
         self.body = [Vector2(0,0),Vector2(0,1),Vector2(0,2),Vector2(0,3)]
 
     def draw_element(self):
         from tetris_pygame import window # place here to avoid the circular import error 
-        from tetris_pygame import square_size
+        from tetris_pygame import square_size, main_window_size
         for element in self.body : 
-            element_rect = pygame.Rect((self.position[0]+element[0])*square_size,(self.position[1]+element[1])*square_size,square_size,square_size)
+            element_rect = pygame.Rect(int(main_window_size*0.625/2)+(self.position[0]+element[0])*square_size,int(main_window_size*0.25/2)+(self.position[1]+element[1])*square_size,square_size,square_size)
             pygame.draw.rect(window,(0,0,0),element_rect)
