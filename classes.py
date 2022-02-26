7249c843b1d1d67f6bf6108dbc3b6da61f7bd4f7
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-02-26 15:32:10 +0100

    Fixing a todo list of object methods

--- classes.py
+++ classes.py
@@ -1,11 +1,13 @@
-import pygame
+import pygame, random
 from pygame.math import Vector2
 
 class ELEMENT:
     """ Will contain element shapes and colors and functions for drawing the elements"""
     def __init__(self) -> None:
         self.position = (4,2)
-        self.body = [Vector2(0,0),Vector2(0,1),Vector2(0,2),Vector2(0,3)]
+        self.element_name = random.choice(["L","L2","I","O","S","Z","T"])
+        self.body = self.element_body()
+        self.color = 
 
     def draw_element(self):
         from tetris_pygame import window # place here to avoid the circular import error 
@@ -13,3 +15,15 @@ class ELEMENT:
         for element in self.body : 
             element_rect = pygame.Rect(int(main_window_size*0.625/2)+(self.position[0]+element[0])*square_size,int(main_window_size*0.25/2)+(self.position[1]+element[1])*square_size,square_size,square_size)
             pygame.draw.rect(window,(0,0,0),element_rect)
+
+    def element_body(self): #decide the element body depending on what element it is
+        pass
+
+    def element_color(self): 
+        pass
+
+
+
+class GAME:
+    def __init__(self) -> None:
+        pass
