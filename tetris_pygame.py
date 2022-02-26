736bc3e8c09a49528c693ee5c86aaf6fff75fa70
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-02-26 12:10:21 +0100

    Adding spaceholders for main area + commenting

--- tetris_pygame.py
+++ tetris_pygame.py
@@ -1,21 +1,40 @@
 import pygame, sys
 
 
-window = pygame.display.set_mode((200,200))
+#main window
+window = pygame.display.set_mode((400,400))
+
+# spaceholders 
+play_space=pygame.Rect(125,50,150,300)
+hold_space = pygame.Rect(50,50,50,50)
+score_space = pygame.Rect(150,10,100,30) 
+next_piece_space = pygame.Rect(300,50,50,50) 
+
+# clock
 clock = pygame.time.Clock()
 
+# screen update timer
 SCREEN_UPDATE = pygame.USEREVENT
 pygame.time.set_timer(SCREEN_UPDATE,500)
 
-
+#game loop
 while True: 
+
     for event in pygame.event.get():
+        # close event
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
-
+    # main window filling 
     window.fill((94,219,167))
 
+    # drawing surfaces
+    pygame.draw.rect(window,(0,0,0),play_space)
+    pygame.draw.rect(window,(0,0,0),hold_space)
+    pygame.draw.rect(window,(0,0,0),score_space)
+    pygame.draw.rect(window,(0,0,0),next_piece_space)
+
+    # update & update frequency
     pygame.display.update()
     clock.tick(30)
