b036b396a449a5846e1f83c0f60c2901ea013e21
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-02-26 13:20:55 +0100

    testing drawing 1 element

--- tetris_pygame.py
+++ tetris_pygame.py
@@ -1,8 +1,10 @@
 import pygame, sys
 
+
 # settings 
 main_window_width = 400
 main_window_height = 400
+square_size = int(main_window_width * 0.0375)
 
 #colors:
 background_color = (94,219,167)
@@ -14,6 +16,10 @@ next_piece_space_color = (255,255,255)
 
 #main window
 window = pygame.display.set_mode((main_window_width,main_window_height))
+
+#test element 
+from classes import ELEMENT # placed after window to avoid the circular import error
+element = ELEMENT()
 
 # spaceholders 
 play_space=pygame.Rect(int(main_window_width*0.625/2),int(main_window_height*0.25/2),int(main_window_width * 0.375),int(main_window_width*0.75))
@@ -45,6 +51,9 @@ while True:
     pygame.draw.rect(window,hold_space_color,hold_space)
     pygame.draw.rect(window,score_background_color,score_space)
     pygame.draw.rect(window,next_piece_space_color,next_piece_space)
+
+    # drawing the element 
+    element.draw_element()
 
     # update & update frequency
     pygame.display.update()
