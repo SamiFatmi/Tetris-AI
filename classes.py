2b658ab9ce2970d5ad996cbc39805410a054dc3d
SamiFatmi <89996292+SamiFatmi@users.noreply.github.com>
2022-02-27 20:58:20 +0100

    Changing the colors and adding a frame

--- classes.py
+++ classes.py
@@ -17,7 +17,7 @@ class ELEMENT:
             element_rect = pygame.Rect(int(main_window_size*0.625/2)+(self.position[0]+element[0])*square_size,int(main_window_size*0.25/2)+(self.position[1]+element[1])*square_size,square_size,square_size)
             pygame.draw.rect(window,self.color,element_rect)
 
-    def element_body(self): #decide the element body depending on what element it is and what orientation it is 
+    def element_body(self): #decide the element body depending on what element and what orientation it is 
         if self.element_name == "T":
             if self.orientation == 1 :
                 self.body = [Vector2(-1,0),Vector2(0,0),Vector2(1,0),Vector2(0,-1)]
@@ -114,14 +114,15 @@ class GAME:
         self.score = 0
 
     def draw_game(self):
-        from tetris_pygame import main_window_size, square_size,window,grid_color,grid_thickeness,game_space_color,hold_space_color,score_background_color,next_piece_space_color,play_space,hold_space,score_space,next_piece_space
+        from tetris_pygame import main_window_size, square_size,window,grid_color,grid_thickeness,game_space_color,hold_space_color,score_background_color,next_piece_space_color,play_space,hold_space,score_space,next_piece_space,stroke_space,stroke_color,stroke_thickeness
 
 
         # drawing surfaces
+        pygame.draw.rect(window,stroke_color,stroke_space)
         pygame.draw.rect(window,game_space_color,play_space)
-        pygame.draw.rect(window,hold_space_color,hold_space)
-        pygame.draw.rect(window,score_background_color,score_space)
-        pygame.draw.rect(window,next_piece_space_color,next_piece_space)
+        pygame.draw.rect(window,hold_space_color,hold_space,stroke_thickeness)
+        pygame.draw.rect(window,score_background_color,score_space,stroke_thickeness)
+        pygame.draw.rect(window,next_piece_space_color,next_piece_space,stroke_thickeness)
 
         #draw space 
         for x in range(10):
@@ -134,9 +135,9 @@ class GAME:
         self.current_element.draw_element()
 
         #draw held and next elemets 
-        self.next_element.position = Vector2(14,3) # position is for test
+        self.next_element.position = Vector2(14,4) # position is for test
         self.next_element.draw_element()
-        self.held_element.position = Vector2(-5,3) # position is for test
+        self.held_element.position = Vector2(-5,4) # position is for test
         self.held_element.draw_element()
 
         #drawing the grid
@@ -147,16 +148,16 @@ class GAME:
 
         #writing Score and HOLD and NEXT
         pygame.font.init()
-        score_font = pygame.font.SysFont('Comic Sans MS', 30)
+        score_font = pygame.font.SysFont('Times New Roman', 30)
         score_textsurface = score_font.render(f"Score : {self.score}", False, (0, 0, 0))
-        next_font = pygame.font.SysFont('Comic Sans MS', 30)
+        next_font = pygame.font.SysFont('Times New Roman', 30)
         next_textsurface = next_font.render('Next', False, (0, 0, 0))
-        hold_font = pygame.font.SysFont('Comic Sans MS', 30)
+        hold_font = pygame.font.SysFont('Times New Roman', 30)
         hold_textsurface = hold_font.render('Hold', False, (0, 0, 0))
 
-        window.blit(score_textsurface,(main_window_size//2.5,main_window_size//32))
-        window.blit(next_textsurface,(main_window_size*0.8,main_window_size//8.5))
-        window.blit(hold_textsurface,(main_window_size//9,main_window_size//8.5))
+        window.blit(score_textsurface,(main_window_size//2.5,main_window_size//25))
+        window.blit(next_textsurface,(main_window_size*0.8,main_window_size//7.5))
+        window.blit(hold_textsurface,(main_window_size//9,main_window_size//7.5))
 
 
 
--- tetris_pygame.py
+++ tetris_pygame.py
@@ -6,14 +6,16 @@ from pygame.constants import KEYDOWN,K_UP,K_DOWN,K_LEFT,K_RIGHT,K_SPACE,K_RSHIFT
 main_window_size= 800
 square_size = int(main_window_size * 0.0375)
 grid_thickeness = 1
+stroke_thickeness = 4
 
 #colors:
-background_color = (94,219,167)
+background_color = (215,238,245)
 game_space_color = (255,255,255)
+stroke_color = (255,0,0)
 hold_space_color = (255,255,255)
 score_background_color = (255,255,255)
 next_piece_space_color = (255,255,255)
-grid_color = (200,200,200)
+grid_color = (230,230,230)
 
 #main window
 window = pygame.display.set_mode((main_window_size,main_window_size))
@@ -24,6 +26,7 @@ main_game = GAME()
 
 # spaceholders 
 play_space=pygame.Rect(int(main_window_size*0.625/2),int(main_window_size*0.25/2),10*square_size,20*square_size)
+stroke_space = pygame.Rect(int(main_window_size*0.625/2)-stroke_thickeness,int(main_window_size*0.25/2)-stroke_thickeness,10*square_size+2*stroke_thickeness,20*square_size+2*stroke_thickeness)
 hold_space = pygame.Rect(main_window_size//16,main_window_size//8,int(main_window_size*0.1875),main_window_size//4)
 score_space = pygame.Rect(int(main_window_size*0.375),int(main_window_size*0.025),int(main_window_size*0.25),int(main_window_size*0.075)) 
 next_piece_space = pygame.Rect(int(main_window_size*0.75),int(main_window_size*0.25/2),int(main_window_size*0.1875),main_window_size//4) 
